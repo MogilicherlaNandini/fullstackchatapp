dbb6760ad8c4d867e00ab3b60c393f386b659d4e
Mogilicherla Nandini <mogilicherlanandini2004@gmail.com>
2025-02-19 13:30:54 +0530

    added files

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,9 @@ dotenv.config({path:"./src/.env"});
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '10mb' }));
+/*app.use(express.json({ limit: '10mb' }));*/
+app.use(express.json({ limit: "500mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173', // Replace with your frontend URL
